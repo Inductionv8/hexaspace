38f74a61f62eb407549df5f6a833528cee9597da
GabrielCastillo <castillog08@gmail.com>
2015-09-10 03:39:30 -0300

    Agregado de algunos import para la redireccion de página y el manejo del usuario. Implementación vaga de homepage.

--- hexaspace/game/views.py
+++ hexaspace/game/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.contrib.auth import authenticate, login, logout, get_user
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+def homepage(request):
+    """
+    Si hay algun usuario, lo redirige a la homepage
+    """
+    user = get_user(request)
+    username = user.username
+    return render_to_response('main.html',{'username':username})
+
